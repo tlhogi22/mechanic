0542d6fb6c14fc2cfdda00c9ac2cf19f8c575555
tlhogi22 <47563661+tlhogi22@users.noreply.github.com>
2019-02-15 10:20:02 +0200

    Update config.php

--- core/config.php
+++ core/config.php
@@ -3,7 +3,7 @@ return[
 	'name' => 'mechanic',
 	'username' => 'user6HJ',
 	'password' => 'Kaysto2#',
-	'connection' => 'mysql:host=mysql',
+	'connection' => 'mysql:host=mysql-mymechanic.7e14.starter-us-west-2.openshiftapps.com',
 	'options' => [
 		PDO::ATTR_PERSISTENT    => true,
     	PDO::ATTR_ERRMODE       => PDO::ERRMODE_EXCEPTION,
